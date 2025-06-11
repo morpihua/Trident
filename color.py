f3a3bb0bf90c2bdce2d43fc10932784630af54f7
morpihua <morpihua@gmail.com>
2025-06-11 05:25:42 +0300

    Update color.py

---
+++ color.py
@@ -0,0 +1,23 @@
+COLORS = {
+    "Red": "#FF0000",
+    "Green": "#00FF00",
+    "Blue": "#0000FF",
+    "White": "#FFFFFF",
+    "Black": "#000000",
+    "Yellow": "#FFFF00",
+    "Magenta": "#FF00FF",
+    "Cyan": "#00FFFF",
+    # додайте всі потрібні
+}
+
+COLORS_EN_UA = {
+    "Red": "Червоний",
+    "Green": "Зелений",
+    "Blue": "Синій",
+    "White": "Білий",
+    "Black": "Чорний",
+    "Yellow": "Жовтий",
+    "Magenta": "Маджента",
+    "Cyan": "Бірюзовий",
+    # додайте всі потрібні
+}
